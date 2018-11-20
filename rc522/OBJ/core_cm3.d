e15cbc8bc14b915d1e0556fa4fca14b7418cc567
zhengguang666 <582635492@qq.com>
2018-11-20 22:49:35 +0800

    init template

---
+++ rc522/OBJ/core_cm3.d
@@ -0,0 +1,2 @@
+..\obj\core_cm3.o: ..\CORE\core_cm3.c
+..\obj\core_cm3.o: D:\SoftWare\keil523\install\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ rc522/OBJ/main.d
@@ -0,0 +1,12 @@
+..\obj\main.o: main.c
+..\obj\main.o: stm32f10x.h
+..\obj\main.o: ..\CORE\core_cm3.h
+..\obj\main.o: D:\SoftWare\keil523\install\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\main.o: system_stm32f10x.h
+..\obj\main.o: stm32f10x_conf.h
+..\obj\main.o: ..\STM32F10x_FWLib\inc\stm32f10x_dbgmcu.h
+..\obj\main.o: ..\USER\stm32f10x.h
+..\obj\main.o: ..\STM32F10x_FWLib\inc\stm32f10x_gpio.h
+..\obj\main.o: ..\STM32F10x_FWLib\inc\stm32f10x_rcc.h
+..\obj\main.o: ..\STM32F10x_FWLib\inc\stm32f10x_usart.h
+..\obj\main.o: ..\STM32F10x_FWLib\inc\misc.h
